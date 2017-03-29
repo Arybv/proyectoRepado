139331804b44b71ed6561d9f357c113e669edd9c
Arantza Burga <arybv18@gmail.com>
2017-03-29 14:39:55 -0500

    avance 2

--- assets/js/cInicio.js
+++ assets/js/cInicio.js
@@ -5,5 +5,19 @@ window.onload=function(){
   var contrasenia=document.getElementsById("contrasenia-coder");
 
   var objetoCoder=JSON.parse(localStorage.getItem("nuevaCoder"));
+    nombre.innerText=objetoCoder.nombre;
+    apellido.innerText=objetoCoder.apellido;
+    correo.innerText=objetoCoder.correo;
+    contrasenia.innerText=objetoCoder.contrasenia;
 
-}
+function Coder(nombreCoder, apellidoCoder, correoCoder, contraseniaCoder){
+  this.nombre=nombreCoder;
+  this.apellido=apellidoCoder;
+  this.correo=correoCoder;
+  this.contrasenia=contraseniaCoder;
+};
+document.getElementsByTagName("editando")[0].addEventListener("click", function(){
+  window.location="dEditar.html";
+})
+
+};
